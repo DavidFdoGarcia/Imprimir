d0016beb5eef75e2b2994dd6258320d8f72c1e01
David <davidfdo.garcia@hotmail.com>
2022-07-17 23:24:16 -0500

    Agregar archivos de proyecto.

---
+++ Imprimir/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Imprimir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            printDocument1 = new PrintDocument();
+            PrinterSettings ps = new PrinterSettings();
+            printDocument1.PrinterSettings = ps;
+            printDocument1.PrintPage += Imprimir;
+            printDocument1.Print();
+        }
+
+        private void Imprimir(object sender, PrintPageEventArgs e)
+        {
+            Font font = new Font("Arial", 14, FontStyle.Regular, GraphicsUnit.Point);
+            int width = 200;
+            int y = 20;
+            e.Graphics.DrawString("Un ticket feliz", font, Brushes.Black, new Rectangle(0, y+=20, width, 20));
+            e.Graphics.DrawString("Un ticket feliz2", font, Brushes.Black, new Rectangle(0, y+=20, width, 20));
+        }
+    }
+}
